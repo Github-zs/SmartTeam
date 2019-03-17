0fd2d2ae5ec6ab0f8e8650f80609fc260abe97c6
zhais <1371007696@qq.com>
2019-03-18 05:44:32 +0800

    init project

---
+++ src/main/java/com/zs/SmartTeam/SmartTeamApplication.java
@@ -0,0 +1,14 @@
+package com.zs.SmartTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTeamApplication.class, args);
+	}
+
+}
+
