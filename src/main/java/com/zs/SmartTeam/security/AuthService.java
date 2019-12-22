ee2d5494639a4db22810a76c0cadc2567cf1f4fd
zhaishuo <1371007696@qq.com>
2019-12-22 08:17:31 +0800

    refactor: refactor security

--- src/main/java/com/zs/SmartTeam/security/AuthService.java
+++
@@ -1,5 +0,0 @@
-package com.zs.SmartTeam.security;
-
-public interface AuthService {
-    String login(String username, String password);
-}
---
+++ src/main/java/com/zs/SmartTeam/security/OAuth2SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.zs.SmartTeam.security;
+
+public class OAuth2SecurityConfig {
+}
